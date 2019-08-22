ffb5ea85c15130b34661b53b42cf2a54e55da9f5
Noah Ruben <noah.ruben@online.de>
2019-08-22 17:29:25 +0200

    base settingsController

--- SoQuaProjektPMS/src/main/java/application/controller/SettingsViewController.java
+++ SoQuaProjektPMS/src/main/java/application/controller/SettingsViewController.java
@@ -6,11 +6,41 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.TextField;
+
 /**
  * @author Admin
  *
  */
 public class SettingsViewController extends Controller {
+
+	@FXML // ResourceBundle that was given to the FXMLLoader
+	private ResourceBundle resources;
+
+	@FXML // URL location of the FXML file that was given to the FXMLLoader
+	private URL location;
+
+	@FXML // fx:id="fileTextField"
+	private TextField fileTextField; // Value injected by FXMLLoader
+
+	@FXML // fx:id="searchButton"
+	private Button searchButton; // Value injected by FXMLLoader
+
+	@FXML // fx:id="dayDropDownButton"
+	private MenuButton dayDropDownButton; // Value injected by FXMLLoader
+
+	@FXML // fx:id="medicationDropDownButton"
+	private MenuButton medicationDropDownButton; // Value injected by FXMLLoader
+
+	@FXML // fx:id="diagrammRadioButton"
+	private CheckBox diagrammRadioButton; // Value injected by FXMLLoader
+
+	@FXML // fx:id="createReportButton"
+	private Button createReportButton; // Value injected by FXMLLoader
 
 	/**
 	 * @param mainController
@@ -27,7 +57,12 @@ public class SettingsViewController extends Controller {
 
 	@Override
 	protected void assertions() {
-		// TODO Auto-generated method stub
+		assert fileTextField != null : "fx:id=\"fileTextField\" was not injected: check your FXML file 'SettingsView.fxml'.";
+        assert searchButton != null : "fx:id=\"searchButton\" was not injected: check your FXML file 'SettingsView.fxml'.";
+        assert dayDropDownButton != null : "fx:id=\"dayDropDownButton\" was not injected: check your FXML file 'SettingsView.fxml'.";
+        assert medicationDropDownButton != null : "fx:id=\"medicationDropDownButton\" was not injected: check your FXML file 'SettingsView.fxml'.";
+        assert diagrammRadioButton != null : "fx:id=\"diagrammRadioButton\" was not injected: check your FXML file 'SettingsView.fxml'.";
+        assert createReportButton != null : "fx:id=\"createReportButton\" was not injected: check your FXML file 'SettingsView.fxml'.";
 	}
 
 	@Override
