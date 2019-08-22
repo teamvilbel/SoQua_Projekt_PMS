f5b8de99d2097b8201ffdfe5b575413205c976f3
Jean-Paul Edoh <jean-pauledoh@web.de>
2019-08-22 19:44:48 +0200

    added fxids to scenes and added annotations to the controllers

--- SoQuaProjektPMS/src/main/java/application/controller/DiagramViewController.java
+++ SoQuaProjektPMS/src/main/java/application/controller/DiagramViewController.java
@@ -6,11 +6,33 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.ToggleButton;
+
 /**
  * @author Admin
  *
  */
 public class DiagramViewController extends Controller {
+	@FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="pieChartCosts"
+    private PieChart pieChartCosts; // Value injected by FXMLLoader
+
+    @FXML // fx:id="pieChartPrescriptions"
+    private PieChart pieChartPrescriptions; // Value injected by FXMLLoader
+
+    @FXML // fx:id="backButton"
+    private Button backButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="toggleButton"
+    private ToggleButton toggleButton; // Value injected by FXMLLoader
 
 	/**
 	 * @param mainController
@@ -28,8 +50,11 @@ public class DiagramViewController extends Controller {
 
 	@Override
 	protected void assertions() {
-		// TODO Auto-generated method stub
-		
+		assert pieChartCosts != null : "fx:id=\"pieChartCosts\" was not injected: check your FXML file 'DiagramView.fxml'.";
+        assert pieChartPrescriptions != null : "fx:id=\"pieChartPrescriptions\" was not injected: check your FXML file 'DiagramView.fxml'.";
+        assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'DiagramView.fxml'.";
+        assert toggleButton != null : "fx:id=\"toggleButton\" was not injected: check your FXML file 'DiagramView.fxml'.";
+
 	}
 
 	@Override
--- SoQuaProjektPMS/src/main/java/application/controller/ReportViewController.java
+++ SoQuaProjektPMS/src/main/java/application/controller/ReportViewController.java
@@ -6,11 +6,52 @@ package application.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 /**
  * @author Admin
  *
  */
 public class ReportViewController extends Controller {
+	@FXML // ResourceBundle that was given to the FXMLLoader
+    private ResourceBundle resources;
+
+    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    private URL location;
+
+    @FXML // fx:id="tableField"
+    private TableView<?> tableField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="dayColumn"
+    private TableColumn<?, ?> dayColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="medicineColumn"
+    private TableColumn<?, ?> medicineColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="prescriptionColumn"
+    private TableColumn<?, ?> prescriptionColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="priceColumn"
+    private TableColumn<?, ?> priceColumn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="backButton"
+    private Button backButton; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sumOfPrescriptions"
+    private TextField sumOfPrescriptions; // Value injected by FXMLLoader
+
+    @FXML // fx:id="sumOfPrices"
+    private TextField sumOfPrices; // Value injected by FXMLLoader
+
+    @FXML // fx:id="informationOutputField"
+    private TextField informationOutputField; // Value injected by FXMLLoader
+
+    @FXML // fx:id="diagramButton"
+    private Button diagramButton; // Value injected by FXMLLoader
 
 	/**
 	 * @param mainController
@@ -28,8 +69,16 @@ public class ReportViewController extends Controller {
 
 	@Override
 	protected void assertions() {
-		// TODO Auto-generated method stub
-		
+		assert tableField != null : "fx:id=\"tableField\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert dayColumn != null : "fx:id=\"dayColumn\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert medicineColumn != null : "fx:id=\"medicineColumn\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert prescriptionColumn != null : "fx:id=\"prescriptionColumn\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert priceColumn != null : "fx:id=\"priceColumn\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert sumOfPrescriptions != null : "fx:id=\"sumOfPrescriptions\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert sumOfPrices != null : "fx:id=\"sumOfPrices\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert informationOutputField != null : "fx:id=\"informationOutputField\" was not injected: check your FXML file 'ReportView.fxml'.";
+        assert diagramButton != null : "fx:id=\"diagramButton\" was not injected: check your FXML file 'ReportView.fxml'.";
 	}
 
 	@Override
