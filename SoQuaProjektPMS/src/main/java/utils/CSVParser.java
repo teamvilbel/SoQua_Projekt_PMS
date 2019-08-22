2f3fb69ba163cd68e7838dfff69a52c8bb717967
Marv <Marv@192.168.13.127>
2019-08-22 17:43:40 +0200

    Test for CSVParser

--- SoQuaProjektPMS/src/main/java/utils/CSVParser.java
+++ SoQuaProjektPMS/src/main/java/utils/CSVParser.java
@@ -10,7 +10,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-
 import beans.MedikamentProTagBean;
 import exceptions.LineCanNotBeParsedException;
 import exceptions.LineIsEmptyException;
@@ -18,118 +17,117 @@ import exceptions.LineIsEmptyException;
 /**
  * @author x
  *         <p>
- *         
+ * 
  *         Lieﬂt CSV-Datei aus und konvertiert die Daten direkt in {@link MedikamentProTagBean}.
  *         <p>
  * 
- *         Der Aufbau der Datei ist: "Datum der
- *         Sprechstunde;Medikamentname;Anzahl der Verschreibungen;Kosten pro
- *         Verschreibung"
+ *         Der Aufbau der Datei ist: "Datum der Sprechstunde;Medikamentname;Anzahl der
+ *         Verschreibungen;Kosten pro Verschreibung"
  * 
  * 
  * @see beans.MedikamentProTagBean
  */
 public class CSVParser {
 
-	/**
-	 * Der Seperator der CSV-Datei
-	 */
-	private static final String SEPERATOR = ";";
-	
-	/**
-	 * Der Sandart Datums-Formattierer.
-	 */
-	private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+  /**
+   * Der Separator der CSV-Datei
+   */
+  private static final String SEPARATOR = ";";
 
-	/**
-	 * Die CSV-Datei mit den Rohdaten.
-	 */
-	private final File csvFile;
+  /**
+   * Der Sandard Datums-Formatierer.
+   */
+  private static final SimpleDateFormat SIMPLEDATEFORMAT = new SimpleDateFormat("dd.MM.yyyy");
+
+  /**
+   * Die CSV-Datei mit den Rohdaten.
+   */
+  private final File csvFile;
 
 
-	/**
-	 * @param csvFile CSV-Datei mit den Rohdaten.
-	 */
-	public CSVParser(File csvFile) {
-		this.csvFile = csvFile;
-	}
+  /**
+   * @param csvFile CSV-Datei mit den Rohdaten.
+   */
+  public CSVParser(File csvFile) {
+    this.csvFile = csvFile;
+  }
 
-	/**
-	 * Lieﬂt CSV-Datei aus und validiert die darin enthaltenen Felder. 
-	 * Konvertiert die Daten direkt in {@link MedikamentProTagBean}.
-	 * 
-	 * @return List of {@link MedikamentProTagBean}
-	 * @throws FileNotFoundException
-	 * @throws IOException
-	 * @throws LineIsEmptyException
-	 * @throws LineCanNotBeParsedException
-	 */
-	public List<MedikamentProTagBean> parseFile() throws FileNotFoundException, IOException, LineIsEmptyException, LineCanNotBeParsedException{
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile));
-		List<MedikamentProTagBean> medikamentProTagBeansList = new ArrayList<MedikamentProTagBean>();
-		
-		try {
-			// to skip the header line.
-			bufferedReader.readLine();
-			String line;
-			// read each line in csv-file
-			while ((line = bufferedReader.readLine()) != null) {
-				// line empty?
-				if (line.isEmpty()) {
-					throw new LineIsEmptyException();
-				} else {
-					// split line 
-					String[] lineSplit = line.split(SEPERATOR);
-					// line has 4 columns
-					if (lineSplit.length == 4) {
-						Date date = null;
-						String name = null;
-						Integer parseInt = null;
-						Double parseDouble = null;
-						try {
-							// parse first column to date
-							date = simpleDateFormat.parse(lineSplit[0]);
-						} catch (ParseException parseException) {
-//							parseException.printStackTrace(); TODO ???
-							throw new LineCanNotBeParsedException("Date field is not valide", parseException);
-						}
-						// second column empty?
-						if (lineSplit[1].trim().isEmpty()) {
-							throw new LineCanNotBeParsedException("Medikamentname field is not valide / empty");
-						}else {
-							name = lineSplit[1].trim();
-						}
-						
-						try {
-							// parse 3. column to integer
-							parseInt = Integer.parseInt(lineSplit[2]);
-						} catch (NumberFormatException numberFormatException) {
-//							numberFormatException.printStackTrace();
-							throw new LineCanNotBeParsedException("Anzverschreibungen field is not valide", numberFormatException);
-						}
-						
-						try {
-							// parse 4. column to double
-							parseDouble = Double.parseDouble(lineSplit[3].replace(',', '.'));
-						} catch (NumberFormatException numberFormatException) {
-//							numberFormatException.printStackTrace();
-							throw new LineCanNotBeParsedException("Einzelpreis field is not valide", numberFormatException);
-						}
-						// add new bean to list
-						medikamentProTagBeansList.add(new MedikamentProTagBean(date, name, parseInt, (Math.round(parseInt * parseDouble * 100.0))/100.0));
-						
-					}else {
-						throw new LineCanNotBeParsedException("Not enough valid fields");
-					}
-				}
-			}
-		} catch (IOException ioException) {
-			// ioException.printStackTrace(); TODO ??
-			throw new IOException("Could not read File");
-		} finally {
-			bufferedReader.close();
-		}
-		return medikamentProTagBeansList;
-	}
+  /**
+   * Lieﬂt CSV-Datei aus und validiert die darin enthaltenen Felder. Konvertiert die Daten direkt in
+   * {@link MedikamentProTagBean}.
+   * 
+   * @return List of {@link MedikamentProTagBean}
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LineIsEmptyException
+   * @throws LineCanNotBeParsedException
+   */
+  public List<MedikamentProTagBean> parseFile()
+      throws FileNotFoundException, IOException, LineIsEmptyException, LineCanNotBeParsedException {
+    BufferedReader bufferedReader = new BufferedReader(new FileReader(csvFile));
+    List<MedikamentProTagBean> medikamentProTagBeansList = new ArrayList<MedikamentProTagBean>();
+
+    try {
+      // to skip the header line.
+      bufferedReader.readLine();
+      String line;
+      // read each line in csv-file
+      while ((line = bufferedReader.readLine()) != null) {
+        // line empty?
+        if (line.isEmpty()) {
+          throw new LineIsEmptyException();
+        } else {
+          // split line
+          String[] lineSplit = line.split(SEPARATOR);
+          // line has 4 columns
+          if (lineSplit.length == 4) {
+            Date date = null;
+            String name = null;
+            Integer parseInt = null;
+            Double parseDouble = null;
+            try {
+              // parse first column to date
+              date = SIMPLEDATEFORMAT.parse(lineSplit[0]);
+            } catch (ParseException parseException) {
+              throw new LineCanNotBeParsedException("Date field is not valide", parseException);
+            }
+            // second column empty?
+            if (lineSplit[1].trim().isEmpty()) {
+              throw new LineCanNotBeParsedException("Medikamentname field is not valide / empty");
+            } else {
+              name = lineSplit[1].trim();
+            }
+
+            try {
+              // parse 3. column to integer
+              parseInt = Integer.parseInt(lineSplit[2]);
+            } catch (NumberFormatException numberFormatException) {
+              throw new LineCanNotBeParsedException("Anzahl Verschreibungen field is not valide",
+                  numberFormatException);
+            }
+
+            try {
+              // parse 4. column to double
+              parseDouble = Double.parseDouble(lineSplit[3].replace(',', '.'));
+            } catch (NumberFormatException numberFormatException) {
+              throw new LineCanNotBeParsedException("Einzelpreis field is not valide",
+                  numberFormatException);
+            }
+            // add new bean to list
+            medikamentProTagBeansList.add(new MedikamentProTagBean(date, name, parseInt,
+                (Math.round(parseInt * parseDouble * 100.0)) / 100.0));
+
+          } else {
+            throw new LineCanNotBeParsedException("Not enough valid fields");
+          }
+        }
+      }
+    } catch (IOException ioException) {
+      throw new IOException("Could not read File");
+    } finally {
+      bufferedReader.close();
+    }
+    return medikamentProTagBeansList;
+  }
 
 }
--- SoQuaProjektPMS/src/test/java/utils/CSVParserTest.java
+++ SoQuaProjektPMS/src/test/java/utils/CSVParserTest.java
@@ -1,67 +1,120 @@
 package utils;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
-
 import beans.MedikamentProTagBean;
 import exceptions.LineCanNotBeParsedException;
 import exceptions.LineIsEmptyException;
-import utils.CSVParser;
 
 class CSVParserTest {
 
-	
+  private static final String path = "SoQuaProjektPMS/src/test/resources/testFiles/";
 
-	@Test
-	void testParseFile() {
-		CSVParser classUnderTest = new CSVParser(new File("./files/testCSV.csv"));
-		try {
-			List<MedikamentProTagBean> parseFile = classUnderTest.parseFile();
-			for (MedikamentProTagBean medikamentProTagBean : parseFile) {
-				System.out.println(medikamentProTagBean);
-			}
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (LineIsEmptyException e) {
-			e.printStackTrace();
-		} catch (LineCanNotBeParsedException e) {
-			e.printStackTrace();
-		}
-	}
-	
+  /**
+   * test for method parseFile
+   * 
+   * @throws FileNotFoundException
+   * @throws IOException
+   * @throws LineIsEmptyException
+   * @throws LineCanNotBeParsedException
+   * @throws ParseException
+   */
+  @Test
+  void testParseFileOneCorrectLine() throws FileNotFoundException, IOException,
+      LineIsEmptyException, LineCanNotBeParsedException, ParseException {
+    CSVParser classUnderTest = new CSVParser(new File(path + "testCSV.csv"));
+    System.out.println(new File(path + "testCSV.csv").getAbsolutePath());
+    List<MedikamentProTagBean> parseFile = classUnderTest.parseFile();
+    assertEquals(parseFile.get(0).getTag(), new SimpleDateFormat("dd.MM.yyyy").parse("01.11.2018"));
+    assertEquals(parseFile.get(0).getMedikamentName(), "Paracetamol");
+    assertEquals(parseFile.get(0).getAnzVerschreibungen(), 5);
+    assertEquals(parseFile.get(0).getGesamtkosten(), 7.45d);
+  }
 
-	@Test
-	void testParseFileOneCorrectLine() throws FileNotFoundException, IOException, LineIsEmptyException, LineCanNotBeParsedException, ParseException {
-		CSVParser classUnderTest = new CSVParser(new File("./files/testCSV.csv"));
-		List<MedikamentProTagBean> parseFile = classUnderTest.parseFile();
-		assertEquals(parseFile.get(0).getTag(), new SimpleDateFormat("dd.MM.yyyy").parse("01.11.2018"));
-		assertEquals(parseFile.get(0).getMedikamentName(), "Paracetamol");
-		assertEquals(parseFile.get(0).getAnzVerschreibungen(), 5);
-		assertEquals(parseFile.get(0).getGesamtkosten(), 7.45d);
-	}
-	
-	@Test
-	void testParseFileInvalideLine(){
-		CSVParser classUnderTest = new CSVParser(new File("./files/testCSV2.csv"));
-		assertThrows(LineCanNotBeParsedException.class	, () -> {
-			classUnderTest.parseFile();
-		});
-	}
-	@Test
-	void testParseDate(){
-		CSVParser classUnderTest = new CSVParser(new File("./files/testCSV2.csv"));
-		assertThrows(LineCanNotBeParsedException.class	, () -> {
-			classUnderTest.parseFile();
-		});
-	}
+  /**
+   * test for method parseFile
+   */
+  @Test
+  void testParseFileNotEnoughValidFields() {
+    CSVParser classUnderTest = new CSVParser(new File(path + "testCSV2.csv"));
+    try {
+      classUnderTest.parseFile();
+      fail("Expected an LineCanNotBeParsedException to be thrown");
+    } catch (LineCanNotBeParsedException e) {
+      assertEquals(e.getMessage(), "Not enough valid fields");
+    } catch (Exception e) {
+      fail("Wrong Exception thrown");
+    }
+  }
+
+  /**
+   * test for method parseFile
+   */
+  @Test
+  void testParseFileWrongDateFormat() {
+    CSVParser classUnderTest = new CSVParser(new File(path + "testCSV3.csv"));
+    try {
+      classUnderTest.parseFile();
+      fail("Expected an LineCanNotBeParsedException to be thrown");
+    } catch (LineCanNotBeParsedException e) {
+      assertEquals(e.getMessage(), "Date field is not valide");
+    } catch (Exception e) {
+      fail("Wrong Exception thrown");
+    }
+  }
+
+  /**
+   * test for method parseFile
+   */
+  @Test
+  void testParseFileNameEmpty() {
+    CSVParser classUnderTest = new CSVParser(new File(path + "testCSV4.csv"));
+    try {
+      classUnderTest.parseFile();
+      fail("Expected an LineCanNotBeParsedException to be thrown");
+    } catch (LineCanNotBeParsedException e) {
+      assertEquals(e.getMessage(), "Medikamentname field is not valide / empty");
+    } catch (Exception e) {
+      fail("Wrong Exception thrown");
+    }
+  }
+
+  /**
+   * test for method parseFile
+   */
+  @Test
+  void testParseFileNumberAsString() {
+    CSVParser classUnderTest = new CSVParser(new File(path + "testCSV5.csv"));
+    try {
+      classUnderTest.parseFile();
+      fail("Expected an LineCanNotBeParsedException to be thrown");
+    } catch (LineCanNotBeParsedException e) {
+      assertEquals(e.getMessage(), "Anzahl Verschreibungen field is not valide");
+    } catch (Exception e) {
+      fail("Wrong Exception thrown");
+    }
+  }
+
+  /**
+   * test for method parseFile
+   */
+  @Test
+  void testParseFilePriceAsString() {
+    CSVParser classUnderTest = new CSVParser(new File(path + "testCSV6.csv"));
+    try {
+      classUnderTest.parseFile();
+      fail("Expected an LineCanNotBeParsedException to be thrown");
+    } catch (LineCanNotBeParsedException e) {
+      assertEquals(e.getMessage(), "Einzelpreis field is not valide");
+    } catch (Exception e) {
+      fail("Wrong Exception thrown");
+    }
+  }
 }
