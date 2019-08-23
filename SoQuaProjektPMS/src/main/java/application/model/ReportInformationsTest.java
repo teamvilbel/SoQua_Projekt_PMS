e0a6caadf8426880ead9f2ac96a0e75fb3845329
Jean-Paul Edoh <jean-pauledoh@web.de>
2019-08-23 12:03:34 +0200

    deleted unitTest

---
+++ SoQuaProjektPMS/src/main/java/application/model/ReportInformationsTest.java
@@ -0,0 +1,152 @@
+/**
+ * 
+ */
+package application.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Jean-Paul Edoh
+ *
+ */
+class ReportInformationsTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#ReportInformations()}.
+	 */
+	@Test
+	final void testReportInformations() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#ReportInformations(javafx.collections.ObservableList, java.util.Date, java.lang.String, boolean)}.
+	 */
+	@Test
+	final void testReportInformationsObservableListOfMedikamentProTagBeanDateStringBoolean() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#getSelectedList()}.
+	 */
+	@Test
+	final void testGetSelectedList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#getMedikamentProTagList()}.
+	 */
+	@Test
+	final void testGetMedikamentProTagList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#getSelectedDay()}.
+	 */
+	@Test
+	final void testGetSelectedDay() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#getSelectedMedicine()}.
+	 */
+	@Test
+	final void testGetSelectedMedicine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#isCreateDiagramm()}.
+	 */
+	@Test
+	final void testIsCreateDiagramm() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#getLastFile()}.
+	 */
+	@Test
+	final void testGetLastFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#setLastFile(java.lang.String)}.
+	 */
+	@Test
+	final void testSetLastFile() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#setCreateDiagramm(boolean)}.
+	 */
+	@Test
+	final void testSetCreateDiagramm() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#setMedikamentProTagList(javafx.collections.ObservableList)}.
+	 */
+	@Test
+	final void testSetMedikamentProTagList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#setSelectedDay(java.util.Date)}.
+	 */
+	@Test
+	final void testSetSelectedDay() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link application.model.ReportInformations#setSelectedMedicine(java.lang.String)}.
+	 */
+	@Test
+	final void testSetSelectedMedicine() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
